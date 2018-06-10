492713996de6606548eb1368707da43404112025
ChangIn <qorckddls010@naver.com>
2018-06-10 20:29:15 +0900

    update detail_view

--- todayPoem/poem/views.py
+++ todayPoem/poem/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Poetry
 
+import datetime
+
 def index(request):
 	#poem_list = Poetry.objects.order_by('poem_title')
 	return render(request, 'poem/pro.html')
 
 def view(request):
-	poem_object = get_object_or_404(Poetry,id=1)
-	return render(request, 'poem/detail.html', {'poem_object':poem_object})
+	#poem_object = get_object_or_404(Poetry,id=1)
+	poem_count = len(Poetry.objects.all())
+	dt = datetime.datetime.now()
+	poem_num = poem_count % dt.day
+	print("count of poem : "+str(poem_count))
+	return render(request, 'poem/pro1.html', {'poem_num':poem_num})
